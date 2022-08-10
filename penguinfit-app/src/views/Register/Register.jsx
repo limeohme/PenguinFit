c9b3d298fe8c62916f9bcfb177ae230cdb753d98
Martin Shukadarov <martinshukadarov@gmail.com>
2022-08-10 21:18:44 +0300

    added: Login and  Register view template

--- penguinfit-app/src/views/Register/Register.jsx
+++ penguinfit-app/src/views/Register/Register.jsx
@@ -1,197 +1,190 @@
-// import './Register.css';
+// import './Login.css';
 // import { useContext, useState } from 'react';
 // import AppState from '../../providers/app-state';
-// import { registerUser, userUpdate } from '../../services/auth-service';
-// import { getRandomAvatar } from '../../services/avatar-service';
-// import { validateRegistration } from '../../utils/validations';
-// import {
-//   createUserHandle,
-//   getUserByHandle,
-// } from '../../services/users-service';
-// import SuccessMessage from './SuccessMessage';
+// import { useLocation, useNavigate } from 'react-router-dom';
+// import { signInUser } from '../../services/auth-service';
+// import { getUserByHandle } from '../../services/users-service';
 // import Error from '../../components/Error/Error';
 // import { keepUserInfo } from '../../services/local-storage-service';
-// import { userRole } from '../../common/user-role';
-// import { formatDateToString } from '../../utils/utils';
 
-// function Register() {
-//   const { appState, setState } = useContext(AppState);
-//   const [show, setShow] = useState(false);
-//   const [errors, setErrors] = useState({
-//     regError: '',
-//   });
-//   const [regMessageTrigger, setTrigger] = useState(false);
-//   const [form, setForm] = useState({
-//     email: '',
-//     password: '',
-//     passwordCheck: '',
-//     username: '',
-//     firstName: '',
-//     lastName: '',
-//   });
+import {
+  Grid,
+  TextField,
+  //   FormControlLabel,
+  //   FormControl,
+  //   FormLabel,
+  //   RadioGroup,
+  //   Radio,
+  //   Select,
+  //   MenuItem,
+  Slider,
+  Button,
+  Box,
+} from '@mui/material';
+  
+import React, { useState } from 'react';
+const defaultValues = {
+  username: '',
+  age: 0,
+  email: '',
+  weight: '',
+  height: 0,
+  phoneNumber: 0,
+  activityStatus: 0,
+  BMI: 0,
 
-//   function formSetter(prop, value) {
-//     setForm({
-//       ...form,
-//       [prop]: value,
-//     });
-//   }
-//   function errorSetter(prop, value) {
-//     setErrors({
-//       ...errors,
-//       [prop]: value,
-//     });
-//   }
-//   function submissionHandler(e) {
-//     e.preventDefault();
-//     try {
-//       validateRegistration(form);
-//       regHandler(form);
-//     } catch (err) {
-//       errorSetter('regError', err.message);
-//     }
-//   }
-//   function regHandler(form) {
-//     getUserByHandle(form.username)
-//       .then((snapshot) => {
-//         if (snapshot.exists()) {
-//           return errorSetter(
-//             'regError',
-//             `User with username ${form.username} already exists!`,
-//           );
-//         }
-//         registerUser(form.email, form.password)
-//           .then((userCredential) => {
-//             // Signed in
-//             const user = userCredential.user;
-
-//             userUpdate(form.username)
-//               .then(() => {
-//                 // const picture = userData.avatarURL || user.photoURL;
-//                 // const userInfo = {
-//                 //   email: user.email,
-//                 //   username: user.displayName,
-//                 //   avatarURL: picture,
-//                 //   handle: userData.handle,
-//                 //   role: userData.role,
-//                 //   firstName: userData.firstName,
-//                 //   lastName: userData.lastName,
-//                 // };
-//                 // createdOn: new Date(),
-//                 // role: userRole.BASIC,
-//                 // avatarURL: '',
-//                 // firstName: userInfo.firstName,
-//                 // lastName: userInfo.lastName
-
-//                 // likedPosts: {},
-
-//                 const userInfo = {
-//                   uid: user.uid,
-//                   email: user.email,
-//                   handle: user.displayName,
-//                   username: user.displayName,
-//                   profilePicture: '',
-//                   avatarURL: getRandomAvatar(user.displayName),
-//                   firstName: form.firstName,
-//                   lastName: form.lastName,
-//                   role: userRole.BASIC,
-//                   createdOn: formatDateToString(new Date()),
-//                 };
-
-//                 createUserHandle(userInfo)
-//                   .then(() => {
-//                     console.log('User successfully added');
-//                     setShow(!show);
-//                     setTrigger(true);
-//                     window.scrollTo(0, 0);
-//                   })
-//                   .catch(console.error);
-
-//                 keepUserInfo(userInfo, userCredential);
-
-//                 setState({
-//                   ...appState,
-//                   user: userInfo,
-//                 });
-//               })
-//               .catch(console.error);
-//           })
-//           .catch((error) => {
-//             const errorMessage = error.message;
-//             if (errorMessage.includes(`email-already-in-use`)) {
-//               errorSetter(
-//                 'regError',
-//                 `Email ${form.email} has already been registered!`,
-//               );
-//             }
-//           });
-//       })
-//       .catch(console.error);
-//   }
-
-//   return (
-//     <>
-//       <SuccessMessage
-//         setTrigger={setTrigger}
-//         regMessageTrigger={regMessageTrigger}
-//       ></SuccessMessage>
-//       <div className="userErrors"></div>
-//       <div className="register">
-//         <div className="inputs">
-//           {/* <h3>Register</h3> */}
-//           <input
-//             type="firstName"
-//             placeholder="First Name*"
-//             onChange={(e) => formSetter('firstName', e.target.value)}
-//           ></input>
-//           <br />
-//           <input
-//             type="lastName"
-//             placeholder="Last Name*"
-//             onChange={(e) => formSetter('lastName', e.target.value)}
-//           ></input>
-//           <br />
-//           <input
-//             type="email"
-//             placeholder="email*"
-//             onChange={(e) => formSetter('email', e.target.value)}
-//           ></input>
-//           <br />
-//           <input
-//             type="username"
-//             placeholder="username*"
-//             onChange={(e) => formSetter('username', e.target.value)}
-//           ></input>
-//           <br />
-//           <input
-//             type="password"
-//             placeholder="password*"
-//             onChange={(e) => formSetter('password', e.target.value)}
-//           ></input>
-//           <br />
-//           <input
-//             type="password"
-//             placeholder="password again...*"
-//             onChange={(e) => formSetter('passwordCheck', e.target.value)}
-//           ></input>
-//           <p className="required">
-//             Fields marked with * are required.
-//             <br /> Yes, I know it's all of them...
-//           </p>
-//           <div className="regErrors">
-//             <Error error={errors.regError || ''}></Error>
-//           </div>
-//           <button
-//             onClick={(e) => {
-//               submissionHandler(e);
-//             }}
-//           >
-//             Register
-//           </button>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Register;
+};
+const Register = () => {
+  const [formValues, setFormValues] = useState(defaultValues);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSliderChange = (name) => (e, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+  return (
+    <>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh' }}
+      >
+  
+        <Grid item xs={3}>
+          <Box>
+            <form onSubmit={handleSubmit}>
+              <Grid container alignItems="center" justify="center" direction="column">
+                <Grid item>
+                  <TextField
+                    id="username-input"
+                    name="username"
+                    label="Username"
+                    type="text"
+                    value={formValues.username}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    id="age-input"
+                    name="age"
+                    label="Age"
+                    type="number"
+                    value={formValues.age}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                {/* username: '',
+  age: 0,
+  email: '',
+  weight: '',
+  height: 0,
+  phoneNumber: 0,
+  activityStatus: 0,
+  BMI: 0, */}
+                <Grid item>
+                  <TextField
+                    id="email-input"
+                    name="email"
+                    label="Email"
+                    type="text"
+                    value={formValues.email}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    id="height-input"
+                    name="height"
+                    label="Height"
+                    type="number"
+                    value={formValues.height}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    id="weight-input"
+                    name="weight"
+                    label="Weight"
+                    type="number"
+                    value={formValues.weight}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    id="phoneNumber-input"
+                    name="phoneNumber"
+                    label="Phone number"
+                    type="number"
+                    value={formValues.phoneNumber}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    id="BMI-input"
+                    name="BMI"
+                    label="BMI"
+                    type="number"
+                    value={formValues.phoneNumber}
+                    onChange={handleInputChange}
+                  />
+                </Grid>
+                <Grid item>
+                  <div style={{ width: '400px' }}>
+              How active are you ?
+                    <Slider
+                      value={formValues.favoriteNumber}
+                      onChange={handleSliderChange('favoriteNumber')}
+                      defaultValue={1}
+                      step={1}
+                      min={1}
+                      max={3}
+                      marks={[
+                        {
+                          value: 1,
+                          label: '1',
+                        },
+                        {
+                          value: 2,
+                          label: '2',
+                        },
+                        {
+                          value: 3,
+                          label: '3',
+                        },
+                      ]}
+                      valueLabelDisplay="off"
+                    />
+                  </div>
+                </Grid>
+                <Button variant="contained" color="primary" type="submit">
+            Submit
+                </Button>
+              </Grid>
+            </form>
+          </Box>
+        </Grid>   
+     
+      </Grid> 
+    </>
+  );
+};
+  
+export default Register;
