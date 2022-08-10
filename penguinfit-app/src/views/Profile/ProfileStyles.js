727038c5ebebdf56240468ae82ec983e4b4f7603
Liya Milenkova <liyakm@gmail.com>
2022-08-10 23:13:27 +0300

    Add: Profile styling in ProfileStyles.js

--- penguinfit-app/src/views/Profile/ProfileStyles.js
+++ penguinfit-app/src/views/Profile/ProfileStyles.js
@@ -1,39 +1,74 @@
-import { lightGreen, purple, pink } from '@mui/material/colors';
+import { lightGreen, purple, pink, indigo } from '@mui/material/colors';
 
-export const green = lightGreen[300];
+export const green = lightGreen[100];
 export const palePink = pink[50];
+export const blue = indigo[400];
+export const deepBlue = indigo[800];
 
 export const wrapperContainerStyle = {
   display: 'flex',
-  flexDirection: 'row',
-  px: 50,
-  py: 5,
+  py: '2rem',
+};
+
+export const userInfoContainer = {
+  display: 'flex',
+  flexDirection: 'column',
+  bgcolor: 'none',
+  my: '5rem',
+  py: '3rem',
+  px: '10rem',
+  borderRadius: 3,
+  border: 4,
+  borderColor: blue
 };
 
 export const midiContainerStyle = {
   display: 'flex',
   flexDirection: 'column',
-  bgcolor: green,
-  my: 10,
-  py: 2,
-  px: 20,
-  width: 'fit-content',
-  borderRadius: 3
+  bgcolor: 'none',
+  my: '5rem',
+  py: '2rem',
+  px: '10rem',
+  ml: '6rem'
 };
 
-export const boxStyle = {
+export const infoBoxStyle = {
   display: 'flex',
   flexDirection: 'row',
   bgcolor: 'background.paper',
   boxShadow: 1,
   borderRadius: 2,
-  p: 2,
-  my: 2,
-  minWidth: 300,
-  maxWidth: 700,
+  border: 1,
+  borderColor: purple[500],
+  p: '1rem',
+  my: '1rem',
+  minWidth: '16rem',
   alignSelf: 'center',
   textAlign: 'center',
   justifyContent: 'space-around'
+};
+
+export const sideBoxStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  boxShadow: 1,
+  borderRadius: 2,
+  border: 4,
+  borderColor: blue,
+  p: '2rem',
+  my: '2rem',
+  alignSelf: 'center',
+  textAlign: 'center',
+  justifyContent: 'space-around',
+  mt: '1.5rem',
+  height: 240,
+  alignItems: 'center'
+};
+
+export const editStyle = {
+  p: 0,
+  m: 0,
+  outline: 0
 };
 
 
@@ -41,8 +76,40 @@ export const avatarStyle = {
   width: 120,
   height: 120,
   alignSelf: 'center',
-  p: 4,
-  border: 1,
-  borderColor: purple,
+  p: '2rem',
+  border: 3,
+  borderColor: purple[800],
   bgcolor: 'white'
+};
+
+export const nameStyle = {
+  fontSize: 24,
+  fontWeight: 700,
+  fontFamily: 'Monospace',
+  letterSpacing: 4,
+
+};
+
+export const ageStyle = {
+  fontSize: 16,
+  fontWeight: 600,
+  fontFamily: 'Monospace',
+  letterSpacing: 4,
+
+};
+
+export const BMIStyle = {
+  fontSize: 58,
+  fontWeight: 700,
+  fontFamily: 'Monospace',
+  letterSpacing: 4,
+  color: purple[700]  
+};
+
+export const activityStyle = {
+  fontSize: 58,
+  fontWeight: 900,
+  fontFamily: 'Monospace',
+  letterSpacing: 4,
+  color: lightGreen[900]
 };
