571870c0ec4bd3c39a60e6ce7012fc807b53cb40
bntsv <you@example.com>
2022-08-10 18:11:30 +0300

    Add: AppBar

--- penguinfit-app/src/App.js
+++ penguinfit-app/src/App.js
@@ -1,27 +1,28 @@
 import './App.css';
 import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 import AppState from './providers/app-state';
 
 import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import NotFound from './views/NotFound/NotFound';
-import Profile from './views/Profile/Profile';
-import Login from './views/Login/Login';
-import Register from './views/Register/Register';
-import Home from './views/Home/Home';
+// import Footer from './components/Footer/Footer';
+
+// import NotFound from './views/NotFound/NotFound';
+// import Profile from './views/Profile/Profile';
+// import Login from './views/Login/Login';
+// import Register from './views/Register/Register';
+// import Home from './views/Home/Home';
+
 // import About from './views/About/About';
 // import Posts from './views/Posts/Posts';
 // import CreatePost from './views/CreatePost/CreatePost';
 // import PostDetailed from './views/PostDetailed/PostDetailed';
-import {
-  getLoggedUser,
-  getLoggedUserAuth,
-} from './services/local-storage-service';
-import Authenticated from './hoc/Authenticated';
-import AuthenticatedAdmin from './hoc/AuthenticatedAdmin';
+import { getLoggedUser, getLoggedUserAuth } from './services/local-storage-service';
+
+// import Authenticated from './hoc/Authenticated';
+// import AuthenticatedAdmin from './hoc/AuthenticatedAdmin';
+
 // import DeletedPost from './views/PostDetailed/DeletedPost/DeletedPost';
 // import UserDetailed from './views/UserDetailed/UserDetailed';
 // import Users from './views/Users/Users';
@@ -29,7 +30,7 @@ import AuthenticatedAdmin from './hoc/AuthenticatedAdmin';
 function App() {
   const [appState, setState] = useState({
     user: getLoggedUser(),
-    userAuthData: getLoggedUserAuth() || null,
+    userAuthData: getLoggedUserAuth() || null
   });
 
   return (
@@ -39,44 +40,16 @@ function App() {
           <Navbar />
 
           <main className="Outlet">
-            <Routes>
+            {/* <Routes>
               <Route index element={<Navigate replace to="home" />} />
               <Route path="home" element={<Home />} />
-              {/* <Route path="about" element={<About />} /> */}
+              <Route path="about" element={<About />} />
               <Route path="login" element={<Login />} />
               <Route path="register" element={<Register />} />
-              <Route
-                path="posts"
-                element={
-                  <Authenticated>
-                    {/* <Posts /> */}
-                  </Authenticated>
-                }
-              />
-              <Route
-                path="create-post"
-                element={
-                  <Authenticated>
-                    {/* <CreatePost /> */}
-                  </Authenticated>
-                }
-              />
-              <Route
-                path="posts/:postId"
-                element={
-                  <Authenticated>
-                    {/* <PostDetailed /> */}
-                  </Authenticated>
-                }
-              />
-              <Route
-                path="deleted"
-                element={
-                  <Authenticated>
-                    {/* <DeletedPost /> */}
-                  </Authenticated>
-                }
-              />
+              <Route path="posts" element={<Authenticated><Posts /></Authenticated>} />
+              <Route path="create-post" element={<Authenticated><CreatePost /></Authenticated>} />
+              <Route path="posts/:postId" element={<Authenticated><PostDetailed /></Authenticated>} />
+              <Route path="deleted" element={<Authenticated><DeletedPost /></Authenticated>} />
               <Route
                 path="profile"
                 element={
@@ -85,26 +58,13 @@ function App() {
                   </Authenticated>
                 }
               />
-              <Route
-                path="users"
-                element={
-                  <AuthenticatedAdmin>
-                    {/* <Users /> */}
-                  </AuthenticatedAdmin>
-                }
-              />
-              <Route
-                path="users/:username"
-                element={
-                  <Authenticated>
-                    {/* <UserDetailed /> */}
-                  </Authenticated>
-                }
-              />
+              <Route path="users" element={<AuthenticatedAdmin><Users /></AuthenticatedAdmin>} />
+              <Route path="users/:username" element={<Authenticated><UserDetailed /></Authenticated>} />
               <Route path="*" element={<NotFound />} />
-            </Routes>
+            </Routes> */}
           </main>
-          <Footer />
+
+          {/* <Footer /> */}
         </div>
       </AppState.Provider>
     </BrowserRouter>
@@ -112,4 +72,3 @@ function App() {
 }
 
 export default App;
-
--- penguinfit-app/src/components/Navbar/Navbar.jsx
+++ penguinfit-app/src/components/Navbar/Navbar.jsx
@@ -1,55 +1,79 @@
-// import './Navbar.css';
+import './Navbar.css';
 // import { useContext } from 'react';
 // import { NavLink } from 'react-router-dom';
 // import AppState from '../../providers/app-state';
-// // import { APP_TITLE } from '../../common/constants';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+// import MenuIcon from '@mui/icons-material/Menu';
+
+// import { APP_TITLE } from '../../common/constants';
 // import { publicPages, userPages } from '../../common/pages';
 // import { renderPublicLinks, renderUserLinks } from '../../utils/utils';
 // import { removeUserFromStorage } from '../../services/local-storage-service';
 // import { userRole } from '../../common/user-role';
 
-// function Navbar() {
+function Navbar() {
 //   const { appState, setState } = useContext(AppState);
-//   const loginLink = (
-//     <NavLink to="/login">
-//       <div className="nav-element">
-//         <p className="text-link" id="login">
-//           Login
-//         </p>
-//       </div>
-//     </NavLink>
-//   );
+  //   const loginLink = (
+  //     <NavLink to="/login">
+  //       <div className="nav-element">
+  //         <p className="text-link" id="login">
+  //           Login
+  //         </p>
+  //       </div>
+  //     </NavLink>
+  //   );
 
-//   const toNavLink = (path, link, id) => {
-//     if (path === 'logout')
-//       return (
-//         <NavLink
-//           to="/"
-//           onClick={() => {
-//             setState({ ...appState, user: null, userData: null });
-//             removeUserFromStorage();
-//           }}
-//           key={id}
-//         >
-//           <div className="nav-element">
-//             <p className="text-link" id={path}>
-//               {link}
-//             </p>
-//           </div>
-//         </NavLink>
-//       );
+  //   const toNavLink = (path, link, id) => {
+  //     // if (path === 'logout')
+  //     //   return (
+  //     //     <NavLink
+  //     //       to="/"
+  //     //       onClick={() => {
+  //     //         setState({ ...appState, user: null, userData: null });
+  //     //         removeUserFromStorage();
+  //     //       }}
+  //     //       key={id}
+  //     //     >
+  //     //       <div className="nav-element">
+  //     //         <p className="text-link" id={path}>
+  //     //           {link}
+  //     //         </p>
+  //     //       </div>
+  //     //     </NavLink>
+  //     //   );
 
-//     return (
-//       <NavLink to={`/${path}`} key={id}>
-//         <div className="nav-element">
-//           <p className="text-link" id={path}>
-//             {link}
-//           </p>
-//         </div>
-//       </NavLink>
-//     );
-//   };
+  //     // return (
+  //     //   <NavLink to={`/${path}`} key={id}>
+  //     //     <div className="nav-element">
+  //     //       <p className="text-link" id={path}>
+  //     //         {link}
+  //     //       </p>
+  //     //     </div>
+  //     //   </NavLink>
+  //     // );
+  //   };
 
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 //   return (
 //     <nav>
 //       <div className="app-title">
@@ -72,6 +96,6 @@
 //       {renderUserLinks(appState.user, loginLink, userPages, toNavLink)}
 //     </nav>
 //   );
-// }
+}
 
-// export default Navbar;
+export default Navbar;
