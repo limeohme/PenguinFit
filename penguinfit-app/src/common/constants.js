76267c6933463f90d5b4656ed41ce2f495e1f60b
bntsv <you@example.com>
2022-08-18 23:24:57 +0300

    Add: GET activities from DB

--- penguinfit-app/src/common/constants.js
+++ penguinfit-app/src/common/constants.js
@@ -1,7 +1,7 @@
 export const DRAWER_WIDTH = 280;
 export const APP_TITLE = 'TheXSpot';
 export const KEYWORD_PREFIX = 'keyword-';
-export const POST_REQUEST_LIMIT = 10;
+export const ACTIVITIES_REQUEST_LIMIT = 7;
 export const LIKE = 'like';
 export const DISLIKE = 'dislike';
 export const LIKE_ICON = '🍓';
@@ -12,7 +12,6 @@ export const BODY_METRICS_RE = /^[0-9]*$/;
 
 export const FOODDATACENTRAL_API_KEY = 'oVhEojYF4Or8pY5R7QqfYh4IfZyD28zdSmIF1ORj';
 // `https://api.nal.usda.gov/fdc/v1/food/${FOOD_ID}?nutrients=203&nutrients=204&nutrients=205&api_key=${FOODDATACENTRAL_API_KEY}`
-export const EDAMAM_APP_ID = '1b47ce33'; 
+export const EDAMAM_APP_ID = '1b47ce33';
 export const EDAMAM_APP_KEY = '75b855ef26da036c00d9fc07c053f84e';
 // `https://api.edamam.com/api/food-database/v2/parser?app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_APP_KEY}&ingr=${FOOD_NAME}&nutrition-type=cooking`
-
