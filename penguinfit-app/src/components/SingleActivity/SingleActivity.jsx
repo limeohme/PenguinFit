dc77e25c91612535cd13b7117464924cce62e823
bntsv <you@example.com>
2022-08-21 18:48:45 +0300

    Update: UI proportions

--- penguinfit-app/src/components/SingleActivity/SingleActivity.jsx
+++ penguinfit-app/src/components/SingleActivity/SingleActivity.jsx
@@ -4,43 +4,51 @@ import { Button, Grid, Paper, Typography } from '@mui/material';
 export default function SingleActivityView({ activity }) {
   
   return (
-    <Paper sx={{ display: 'flex' , backgroundColor: '#6633ff10', p: 2 }}>
-      {/* <Card sx={{ display: 'flex' , backgroundColor: '#6633ff10' }}>
-        <CardActionArea>
-          <CardContent> */}
+    <Paper sx={{ 
+      display: 'flex' ,
+      backgroundColor: '#6633ff10', p: 2, minHeight: '11em' }}>
 
-      <Grid container direction='column' spacing={1} justifyContent='space-between' alignItems='center'>
-        <Grid item container spacing={2} justifyContent='space-between' alignItems='center'>
-          <Grid item xs={6} sm={6} >
+      <Grid container direction='column' columnGap={0} justifyContent='space-between' alignItems='center'>
+
+        <Grid item container justifyContent='space-between' alignItems='center'>
+
+          <Grid item xs={5.5} sm={4} >
             <Typography variant='h5'>{activity.title}</Typography>
           </Grid>
-          <Grid item xs={6} sm={6}>
+
+          <Grid item xs={6.5} sm={8}>
             <Typography sx={{ textAlign:'right' }}>{activity.createdOn}</Typography>
+            <Typography sx={{ textAlign:'right' }}>{activity?.createdAt}</Typography>
           </Grid>
+
         </Grid>
 
-        <Grid item container spacing={2} justifyContent='space-between' alignItems='centre' sx={{ color:'#00000050' }}>
+        <Grid item container justifyContent='space-between' alignItems='centre' sx={{ color:'#00000050' }}>
+
           <Grid item xs={6} sm={6}>
             <Typography>{`${activity.distance} km, ${activity.type}`}</Typography>
           </Grid>
+
           <Grid item xs={6} sm={6}>
             <Typography sx={{ textAlign:'right' }}><em>{activity.buddy? `with ${activity.buddy}` : null}</em></Typography>
           </Grid>
+
         </Grid>
 
-        <Grid item container spacing={2} justifyContent='space-between'  alignItems='center'>
-          <Grid item xs={6} sm={6}>
+        <Grid item container justifyContent='space-between'  alignItems='center'>
+
+          <Grid item xs={8} sm={8}>
             <Typography variant="h6">{`${activity.duration} min, ${activity.calories.toFixed(0)} kcal`}</Typography>
           </Grid>
-          <Grid container item xs={6} sm={6} justifyContent='right'>
+
+          <Grid container item xs={4} sm={4} justifyContent='right'>
             <Button variant="text" color="primary" size='small' sx={{ ml:'auto' }}>ADD</Button>
           </Grid>
-        </Grid>
-      </Grid>
 
-      {/* </CardContent>
-        </CardActionArea>
-      </Card> */}
+        </Grid>
+
+      </Grid>
+      
     </Paper>
   );
 
