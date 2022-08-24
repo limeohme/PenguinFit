f417718e39783104838c3730f143d2b8c3b6c97b
Martin Shukadarov <martinshukadarov@gmail.com>
2022-08-24 23:25:15 +0300

    updated: request service to friendRequest service

--- penguinfit-app/src/services/friends-service.js
+++ penguinfit-app/src/services/friends-service.js
@@ -4,20 +4,20 @@ import { db } from '../config/firebase-config';
 export const sendFriendRequest = (sender, receiver) => {
   const request = createFriendRequest(sender, receiver);
   
-  get(ref(db, `requests/${sender}/${receiver}`)).then((snapshot) => {
+  get(ref(db, `friendsRequests/${sender}/${receiver}`)).then((snapshot) => {
     const sentRequests = snapshot.val() || {};
-    update(ref(db, `requests/${sender}/${receiver}`), { ...sentRequests, ...request });
+    update(ref(db, `friendsRequests/${sender}/${receiver}`), { ...sentRequests, ...request });
   });
 
-  get(ref(db, `requests/${receiver}/${sender}`)).then((snapshot) => {
+  get(ref(db, `friendsRequests/${receiver}/${sender}`)).then((snapshot) => {
     const receivedRequests = snapshot.val() || {};
-    update(ref(db, `requests/${receiver}/${sender}`), { ...receivedRequests, ...request });
+    update(ref(db, `friendsRequests/${receiver}/${sender}`), { ...receivedRequests, ...request });
   });
 };
 
 export const deleteFriendRequest = (sender, receiver) => {
-  remove(ref(db, `requests/${sender}/${receiver}`));
-  remove(ref(db, `requests/${receiver}/${sender}`));
+  remove(ref(db, `friendsRequests/${sender}/${receiver}`));
+  remove(ref(db, `friendsRequests/${receiver}/${sender}`));
 };
 
 export const acceptFriendRequest = (sender, receiver) => {
@@ -29,14 +29,14 @@ export const acceptFriendRequest = (sender, receiver) => {
 const addFriend = (handle, friend) => {
   get(ref(db, `users/${handle}/friends`)).then((snapshot) => {
     const userFriends = snapshot.val() || {};
-    console.log(handle, friend , { ...userFriends, [friend]:true });
+    // console.log(handle, friend , { ...userFriends, [friend]:true });
     set(ref(db, `users/${handle}/friends`), { ...userFriends, [friend]:true });
   });
 };
 
 export const listenToRequests = (handle, listen) => {
   // console.log(`requests/${handle}`);
-  return onValue(ref(db, `requests/${handle}`), listen);
+  return onValue(ref(db, `friendsRequests/${handle}`), listen);
 };
 
 const createFriendRequest = (sender, receiver) => {
@@ -62,5 +62,5 @@ const removeFriend = (handle, friend) => {
 };
 
 export const getUserRequest = (handle) => {
-  return get(ref(db, `requests/${handle}`));
+  return get(ref(db, `friendsRequests/${handle}`));
 };
