0f460a3f7211dbfca9ec464c4e9f20a59ac044d4
bntsv <you@example.com>
2022-08-24 23:36:54 +0300

    Update: change APP_TITTLE + comment out some vars

--- penguinfit-app/src/common/constants.js
+++ penguinfit-app/src/common/constants.js
@@ -1,11 +1,12 @@
 export const DRAWER_WIDTH = 280;
-export const APP_TITLE = 'TheXSpot';
+export const APP_TITLE = 'PenguinFit';
 export const KEYWORD_PREFIX = 'keyword-';
 export const ACTIVITIES_REQUEST_LIMIT = 7;
-export const LIKE = 'like';
-export const DISLIKE = 'dislike';
-export const LIKE_ICON = '🍓';
-export const DISLIKE_ICON = '🍋';
+
+// export const LIKE = 'like';
+// export const DISLIKE = 'dislike';
+// export const LIKE_ICON = '🍓';
+// export const DISLIKE_ICON = '🍋';
 
 export const mailRE = /[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
 export const BODY_METRICS_RE = /^[0-9]*$/;
