bbfb2030d738aeb91576be976a9c7146ff11d2b7
Martin Shukadarov <martinshukadarov@gmail.com>
2022-08-26 19:13:08 +0300

    Updated: create goal obj

--- penguinfit-app/src/utils/utils.js
+++ penguinfit-app/src/utils/utils.js
@@ -93,6 +93,14 @@ export const toSnakeCase = (string) => {
   return string;
 };
 
+export function toCamelCase(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+    if (+match === 0) return ''; // or if (/\s+/.test(match)) for white spaces
+    return index === 0 ? match.toLowerCase() : match.toUpperCase();
+  });
+}
+
+
 export const isCyrillic = (string) => {
   return /[а-яА-ЯЁё]/.test(string);
 };
