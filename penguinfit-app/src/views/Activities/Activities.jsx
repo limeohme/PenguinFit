8c09337497f0687ce0cc44e89e3e456ebb6c77fa
bntsv <you@example.com>
2022-08-27 22:09:21 +0300

    Refactor: use async/await with try/catch

--- penguinfit-app/src/views/Activities/Activities.jsx
+++ penguinfit-app/src/views/Activities/Activities.jsx
@@ -78,7 +78,9 @@ function Activities() {
     const unsubscribe = getLiveUserActivities(user.username, async () => {
       try{
         const recent = await getMostRecentUserActivities(user.username);
+
         setActivities(recent);
+
       }catch(err){
         console.error(err);
       }
@@ -89,22 +91,21 @@ function Activities() {
 
   useEffect(() => {
     const unsubscribe = getLiveActivityRequests(user.username, async (snapshot) => {
-      if (snapshot.exists()) {
-        const requestsArr = getActivityRequestsArray(snapshot);
-        getUserActivitiesFromRequests(requestsArr)
-          .then((result)=>{
-            setActivityRequests(result);
-          });
-      } else {
-        setActivityRequests([]);
-      }
 
-      // try{
-      //   const recent = await getMostRecentUserActivities(user.username, ACTIVITIES_REQUEST_LIMIT);
-      //   setActivities(recent);
-      // }catch(err){
-      //   console.error(err);
-      // }
+      try{
+        if (snapshot.exists()) {
+          const requestsArr = getActivityRequestsArray(snapshot);
+          const requested = await getUserActivitiesFromRequests(requestsArr);
+  
+          setActivityRequests(requested);
+          
+        } else {
+  
+          setActivityRequests([]);
+        }
+      }catch(err){
+        console.error(err);
+      }
     });
 
     return () => unsubscribe();
@@ -139,7 +140,7 @@ function Activities() {
 
       <Grid item xs={12} sm={6.5}>
         <Typography variant='h5' sx={{ pb:2 }}>Statistics:</Typography>
-        {/* <DetailedGoalsStepper steps={ steps }></DetailedGoalsStepper> */}
+        
         <Grid 
           container
           item
