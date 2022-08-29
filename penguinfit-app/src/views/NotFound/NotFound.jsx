d799b2694878fe84109eea9be69b4a9d45f94bf5
Liya Milenkova <liyakm@gmail.com>
2022-08-29 19:57:58 +0300

    Update: NotFound page

--- penguinfit-app/src/views/NotFound/NotFound.jsx
+++ penguinfit-app/src/views/NotFound/NotFound.jsx
@@ -1,5 +1,15 @@
-import './NotFound.css';
+import { Card, CardMedia } from '@mui/material';
 
 export default function NotFound() {
-  return <h1 className="not-found">404</h1>;
+  return (
+    <Card sx={{ backgroundColor: 'transparent', p: 2 }} elevation={0}>
+      <CardMedia
+        component="img"
+        height={'600vh'}
+        image={require('../../images/404Pingu.png')}
+        alt="penguin"
+        sx={{ objectFit: 'scale-down', my: '4rem' }}
+      />
+    </Card>
+  );
 }
