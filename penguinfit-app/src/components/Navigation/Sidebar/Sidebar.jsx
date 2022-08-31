923ba1b9429bdae2d1dea03d7cc12b08ee76efe6
bntsv <you@example.com>
2022-09-01 00:28:43 +0300

    Update: sx

--- penguinfit-app/src/components/Navigation/Sidebar/Sidebar.jsx
+++ penguinfit-app/src/components/Navigation/Sidebar/Sidebar.jsx
@@ -1,11 +1,9 @@
-import { useLocation } from 'react-router-dom';
 import { Drawer, List, ListItemButton, ListItemText, Toolbar } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { sidebarPages } from '../../../common/sidebar-pages';
 
 function Sidebar({ classes, toggleDrawer, open, isMdUp }) {
   const { pathname } = useLocation();
-  console.log(pathname);
 
   return (
     <Drawer
@@ -18,7 +16,7 @@ function Sidebar({ classes, toggleDrawer, open, isMdUp }) {
       <Toolbar />
       <List>
         {Object.entries(sidebarPages).map(([key, value]) => (
-          <ListItemButton button key={key} component={NavLink} to={key} selected={pathname === `/private/${key}`}>
+          <ListItemButton key={key} component={NavLink} to={key} selected={pathname === `/private/${key}`} sx={classes.sideLink}>
             <ListItemText primary={value} />
           </ListItemButton>
         ))}
