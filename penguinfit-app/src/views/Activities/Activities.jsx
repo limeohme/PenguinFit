dce2ef24f7a62f8dc54393cd74c572b7e4e1109d
bntsv <you@example.com>
2022-08-31 20:01:34 +0300

    Add: get data dynamically

--- penguinfit-app/src/views/Activities/Activities.jsx
+++ penguinfit-app/src/views/Activities/Activities.jsx
@@ -8,7 +8,7 @@ import { Box, Grid, Paper, Typography } from '@mui/material';
 import CreateActivityForm from '../../components/FormsComponents/CreateActivityForm/CreateActivityForm';
 import DisplayActivities from '../../components/ListComponents/DisplayActivities/DisplayActivities';
 import DisplayActivityRequests from '../../components/ListComponents/DisplayActivityRequests/DisplayActivityRequests';
-import { getActivitiesDataByType, getFieldByType, getLiveUserLastNDaysData } from '../../services/data-viz-service';
+import { formatCaloriesBurnedByDay, getActivitiesDataByType, getCaloriesBurnedByDay, getFieldByType, getLiveUserLastNDaysData } from '../../services/data-viz-service';
 import { PieChartActivityTypes } from '../../components/DataVisualisationComponents/PieCharts/PieCharts';
 import { CaloriesToDurationByActivityTypeBar } from '../../components/DataVisualisationComponents/BarCharts/ActivityBarCharts';
 import { CaloriesBurnedByDay } from '../../components/DataVisualisationComponents/BarCharts/CaloriesBurnedByDay';
@@ -20,6 +20,9 @@ function Activities() {
   const [activities, setActivities] = useState([]);
   const [activityRequests, setActivityRequests] = useState([]);
   const [countByType, setCountByType] = useState({});
+  const [durationByType, setDurationByType] = useState({});
+  const [caloriesByType, setCaloriesByType] = useState({});
+  const [dailyCaloriesBurned, setDailyCaloriesBurned] = useState([]);
 
   useEffect(() => {
     const unsubscribe = getLiveUserActivities(user.username, async () => {
@@ -39,18 +42,35 @@ function Activities() {
   useEffect(() => {
     const unsubscribe = getLiveUserLastNDaysData(user.username, (snapshot) => {
       try{
-        const lastNDaysData = Object.values(snapshot.val());
+        if(snapshot.exists()){
+          const lastNDaysData = Object.values(snapshot.val());
+          console.log(lastNDaysData);
 
-        // obj/activites/countByType
-        const activitiesDataByType = getActivitiesDataByType(lastNDaysData);
-        const typesByCount = getFieldByType(activitiesDataByType, 'countOfType');
-        setCountByType(typesByCount);
-        // console.log(countByType);
+          // obj/cals/burned
 
-        // obj/cals/burned
+          const caloriesBurnedByDay = getCaloriesBurnedByDay(lastNDaysData);
+          const formattedCaloriesBurnedByDay = formatCaloriesBurnedByDay(caloriesBurnedByDay);
+          setDailyCaloriesBurned(formattedCaloriesBurnedByDay);
 
 
-        // obj/activites/durationByType & caloriesByType
+          const activitiesDataByType = getActivitiesDataByType(lastNDaysData);
+
+          // obj/activities/countByType
+          const typesByCount = getFieldByType(activitiesDataByType, 'countOfType');
+          setCountByType(typesByCount);
+
+          // obj/activities/durationByType
+          const typesByDuration = getFieldByType(activitiesDataByType, 'durationOfType');
+          setDurationByType(typesByDuration);
+          console.log(durationByType);
+
+          // obj/activities/caloriesByType
+          const typesByCalories = getFieldByType(activitiesDataByType, 'caloriesOfType');
+          setCaloriesByType(typesByCalories);
+          console.log(caloriesByType);
+
+        }
+        
 
 
       }catch(err){
@@ -93,8 +113,10 @@ function Activities() {
       spacing={4}
     >
       <Grid container item direction="column" gap={4} xs={12} sm={5}>
+
         <Grid container item direction="column">
           <Typography variant='h5' sx={{ pb:2 }}>New activity:</Typography>
+
           <Paper sx={{ backgroundColor: '#ffffff' }}>
             <CreateActivityForm />
           </Paper>
@@ -120,8 +142,6 @@ function Activities() {
           justifyContent="center"
           alignItems="center"
           gap={4}
-        //   sx={{ p:4 }}
-        //   spacing={4}
         >
           <Grid container item xs={12} sm={12}>
             <Paper sx={{ backgroundColor: '#ffffff75',  p: 1, width: '100%' }}>
@@ -130,21 +150,25 @@ function Activities() {
                 <Typography sx={{ fontSize: 15 }}> calories burned by day </Typography>
                 <Typography sx={{ fontSize: 10 }}> (last 14 days) </Typography>
               </Box>
+              <CaloriesBurnedByDay  caloriesBurnedByDay={dailyCaloriesBurned}></CaloriesBurnedByDay>
 
-              <CaloriesBurnedByDay></CaloriesBurnedByDay>
             </Paper>
           </Grid>
+
           <Grid container item spacing={4} xs={12} sm={12}>
+
             <Grid item xs={12} sm={6}>
               <Paper sx={{ backgroundColor: '#ffffff75',  p: 1  }}>
+
                 <Box>
                   <Typography sx={{ fontSize: 15 }}> activity types in % </Typography>
                   <Typography sx={{ fontSize: 10 }}> (last 14 days) </Typography>
                 </Box>
-                
                 <PieChartActivityTypes countByType={countByType}></PieChartActivityTypes>
+
               </Paper>
             </Grid>
+
             <Grid item xs={12} sm={6}>
               <Paper sx={{ backgroundColor: '#ffffff75',  p: 1  }}>
                 
@@ -152,11 +176,15 @@ function Activities() {
                   <Typography sx={{ fontSize: 15 }}> duration vs. calories burned by type </Typography>
                   <Typography sx={{ fontSize: 10 }}> (last 14 days) </Typography>
                 </Box>
-                
-                <CaloriesToDurationByActivityTypeBar ></CaloriesToDurationByActivityTypeBar>
+                <CaloriesToDurationByActivityTypeBar 
+                  dataByType={[durationByType, caloriesByType]}
+                ></CaloriesToDurationByActivityTypeBar>
+
               </Paper>
             </Grid>
+
           </Grid>
+
         </Grid>
         
       </Grid>
